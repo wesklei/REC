76e2e68b3f02d794fec5e02651402dbe486cfc31
Wesklei <wesklei.m@gmail.com>
2014-11-13 09:24:03 -0200

    sleep no main

--- REC_Lab7/src/main/java/com/br/rec_lab7/main/Main.java
+++ REC_Lab7/src/main/java/com/br/rec_lab7/main/Main.java
@@ -7,13 +7,13 @@ package com.br.rec_lab7.main;
 
 /**
  *
- * @author zanatta
+ * @author Wesklei Migliorini <wesklei.m at gmail dt com>
  */
 public class Main {
     public static void main(String argv[]){
        // Conexao conexao = new Conexao();
         //conexao.start();
-        SplashScreen splash = new SplashScreen(1000);
+        SplashScreen splash = new SplashScreen(5000);
         splash.shows();
 
         java.awt.EventQueue.invokeLater(new Runnable() {
